37ab109de5fcf288c8fe9849e65da114ae215468
hugosilvag6 <hugosilvag6@hotmail.com>
2022-07-08 17:42:18 -0300

    Initial Commit

---
+++ BetterRest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Hugo Silva on 08/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
